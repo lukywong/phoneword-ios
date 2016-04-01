43011bcbd97bd5bc3635fc54bf2340ce101adc92
lukywong <lukywong@gmail.com>
2016-04-01 17:13:38 +0800

    Change to use push view control instead of segue.

--- Phoneword_iOS/CallHistoryController.cs
+++ Phoneword_iOS/CallHistoryController.cs
@@ -6,46 +6,47 @@ using UIKit;
 
 namespace Phoneword_iOS
 {
-	partial class CallHistoryController : UITableViewController
-	{
-		public List<String> PhoneNumbers { get; set; }
+    partial class CallHistoryController : UITableViewController
+    {
+        public List<String> PhoneNumbers { get; set; }
 
-		static NSString callHistoryCellId = new NSString ("CallHistoryCell");
+        static NSString callHistoryCellId = new NSString("CallHistoryCell");
 
-		public CallHistoryController (IntPtr handle) : base (handle)
-		{
-			TableView.RegisterClassForCellReuse (typeof(UITableViewCell), callHistoryCellId);
-			TableView.Source = new CallHistoryDataSource (this);
-			PhoneNumbers = new List<string> ();
-		}
+        public CallHistoryController(IntPtr handle)
+            : base(handle)
+        {
+            TableView.RegisterClassForCellReuse(typeof(UITableViewCell), callHistoryCellId);
+            TableView.Source = new CallHistoryDataSource(this);
+            PhoneNumbers = new List<string>();
+        }
 
-		class CallHistoryDataSource : UITableViewSource
-		{
-			CallHistoryController controller;
+        class CallHistoryDataSource : UITableViewSource
+        {
+            CallHistoryController controller;
 
-			public CallHistoryDataSource (CallHistoryController controller)
-			{
-				this.controller = controller;
-			}
+            public CallHistoryDataSource(CallHistoryController controller)
+            {
+                this.controller = controller;
+            }
 
-			// Returns the number of rows in each section of the table
-			public override nint RowsInSection (UITableView tableView, nint section)
-			{
-				return controller.PhoneNumbers.Count;
-			}
-			//
-			// Returns a table cell for the row indicated by row property of the NSIndexPath
-			// This method is called multiple times to populate each row of the table.
-			// The method automatically uses cells that have scrolled off the screen or creates new ones as necessary.
-			//
-			public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
-			{
-				var cell = tableView.DequeueReusableCell (CallHistoryController.callHistoryCellId);
+            // Returns the number of rows in each section of the table
+            public override nint RowsInSection(UITableView tableView, nint section)
+            {
+                return controller.PhoneNumbers.Count;
+            }
+            //
+            // Returns a table cell for the row indicated by row property of the NSIndexPath
+            // This method is called multiple times to populate each row of the table.
+            // The method automatically uses cells that have scrolled off the screen or creates new ones as necessary.
+            //
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                var cell = tableView.DequeueReusableCell(CallHistoryController.callHistoryCellId);
 
-				int row = indexPath.Row;
-				cell.TextLabel.Text = controller.PhoneNumbers [row];
-				return cell;
-			}
-		}
-	}
+                int row = indexPath.Row;
+                cell.TextLabel.Text = controller.PhoneNumbers[row];
+                return cell;
+            }
+        }
+    }
 }
--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -6,79 +6,97 @@ using Foundation;
 
 namespace Phoneword_iOS
 {
-	public partial class ViewController : UIViewController
-	{
-		string translatedNumber = "";
+    public partial class ViewController : UIViewController
+    {
+        string translatedNumber = "";
 
-		public List<String> PhoneNumbers { get; set; }
+        public List<String> PhoneNumbers { get; set; }
 
-		public ViewController (IntPtr handle) : base (handle)
-		{
-			PhoneNumbers = new List<String> ();
-		}
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+            PhoneNumbers = new List<String>();
+        }
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
 
-			string translatedNumber = "";
+            string translatedNumber = "";
 
-			TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
-				// Convert the phone number with text to a number
-				// using PhoneTranslator.cs
-				translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Convert the phone number with text to a number
+                // using PhoneTranslator.cs
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
 
-				// Dismiss the keyboard if text field was tapped
-				PhoneNumberText.ResignFirstResponder ();
+                // Dismiss the keyboard if text field was tapped
+                PhoneNumberText.ResignFirstResponder();
 
-				if (translatedNumber == "") {
-					CallButton.SetTitle ("Call ", UIControlState.Normal);
-					CallButton.Enabled = false;
-				} else {
-					CallButton.SetTitle ("Call " + translatedNumber,
-						UIControlState.Normal);
-					CallButton.Enabled = true;
-				}
-			};
-			CallButton.TouchUpInside += (object sender, EventArgs e) => {
-				//Store the phone number that we're dialing in PhoneNumbers
-				PhoneNumbers.Add (translatedNumber);
+                if (translatedNumber == "")
+                {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle("Call " + translatedNumber,
+                        UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+            CallButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                //Store the phone number that we're dialing in PhoneNumbers
+                PhoneNumbers.Add(translatedNumber);
 
-				// Use URL handler with tel: prefix to invoke Apple's Phone app...
-				var url = new NSUrl ("tel:" + translatedNumber);
+                // Use URL handler with tel: prefix to invoke Apple's Phone app...
+                var url = new NSUrl("tel:" + translatedNumber);
 
-				// ...otherwise show an alert dialog
-				if (!UIApplication.SharedApplication.OpenUrl (url)) {
-					var alert = UIAlertController.Create ("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
-					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
-					PresentViewController (alert, true, null);
-				}
-			};
-		}
+                // ...otherwise show an alert dialog
+                if (!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+                }
+            };
+            CallHistoryButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Launches a new instance of CallHistoryController
+                CallHistoryController callHistory = 
+                    this.Storyboard.InstantiateViewController("CallHistoryController") as CallHistoryController;
+                if (callHistory != null)
+                {
+                    callHistory.PhoneNumbers = PhoneNumbers;
+                    this.NavigationController.PushViewController(callHistory, true);
+                }
+            };
+        }
 
-		public override void PrepareForSegue (UIStoryboardSegue segue, NSObject sender)
-		{
-			base.PrepareForSegue (segue, sender);
+        //		public override void PrepareForSegue (UIStoryboardSegue segue, NSObject sender)
+        //		{
+        //			base.PrepareForSegue (segue, sender);
+        //
+        //			// set the View Controller that’s powering the screen we’re
+        //			// transitioning to
+        //
+        //			var callHistoryContoller = segue.DestinationViewController as CallHistoryController;
+        //
+        //			//set the Table View Controller’s list of phone numbers to the
+        //			// list of dialed phone numbers
+        //
+        //			if (callHistoryContoller != null) {
+        //				callHistoryContoller.PhoneNumbers = PhoneNumbers;
+        //			}
+        //		}
 
-			// set the View Controller that’s powering the screen we’re
-			// transitioning to
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
 
-			var callHistoryContoller = segue.DestinationViewController as CallHistoryController;
-
-			//set the Table View Controller’s list of phone numbers to the
-			// list of dialed phone numbers
-
-			if (callHistoryContoller != null) {
-				callHistoryContoller.PhoneNumbers = PhoneNumbers;
-			}
-		}
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
-		}
-
-	}
+    }
 }
